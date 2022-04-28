de6f074703b21b0bc79c32f2933d11b80b216822
Valentine Garbuzenko <nickf@tut.by>
2022-04-28 16:08:27 +0300

    feat: "carbon-dating" task done

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,9 +17,16 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+
+function dateSample(sampleActivity) {
+  // formula: N(t)=N(0)*exp(-lam * t), T(1/2) = ln(2) / lam, => N(t)=N(0)*2**(-t/T(1/2)) =>
+  // t = -T(1/2)*log2(N(t)/N(0));
+  let result = false;
+  if((typeof(sampleActivity) !== 'string') || isNaN(sampleActivity)) return result;
+  // if(!isFinite(sampleActivity)) return result;
+  if((+sampleActivity <= 0) || (+sampleActivity > MODERN_ACTIVITY)) return result;
+  result = Math.ceil(HALF_LIFE_PERIOD * Math.log2(MODERN_ACTIVITY/+sampleActivity));
+  return result;
 }
 
 module.exports = {
