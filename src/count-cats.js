5a15fca7692345115583d99e78139d1f843dbc2f
Valentine Garbuzenko <nickf@tut.by>
2022-04-28 14:11:58 +0300

    feat: "count-cats" task done

--- src/count-cats.js
+++ src/count-cats.js
@@ -14,10 +14,29 @@ const { NotImplementedError } = require('../extensions/index.js');
  * ]) => 3`
  *
  */
-function countCats(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
+// let arrWithCats =
+//   [
+//     [0, 1, '^^'],
+//     [0, '^^', 2],
+//     ['^^', 1, 2]
+//   ]
+
+function countCats(inArr) {
+  let catsSum = 0;
+  const catElm = '^^';
+  // throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
+  for(let i = 0; i < inArr.length; i++) {
+    for(let j = 0; j < inArr[0].length; j++) {
+      if(inArr[i][j] === catElm) catsSum++;
+      // console.log(inArr[i][j]);
+    }
+  }
+  return catsSum;
 }
+
+// const sum = countCats(arrWithCats);
+// console.log(arrWithCats.length, arrWithCats, sum);
 
 module.exports = {
   countCats
