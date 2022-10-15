5409ddda6234b6544305d2f551705b257f55bb4b
NickFallman <nickf@tut.by>
2022-10-15 18:03:18 +0300

    refactor: "recursive-depth" task remove comments

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -12,12 +12,6 @@ const { NotImplementedError } = require('../extensions/index.js');
  * depthCalc.calculateDepth([[[]]]) => 3
  *
  */
-// class DepthCalculator {
-//   calculateDepth(/* arr */) {
-//     throw new NotImplementedError('Not implemented');
-//     // remove line with error and write your code here
-//   }
-// }
 class DepthCalculator {
   calculateDepth( arr ) {
     let depthCounter = 0;
