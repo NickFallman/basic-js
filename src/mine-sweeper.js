6c2f34cd48477429d760833a50cbc62d5d2f3081
NickFallman <nickf@tut.by>
2022-10-15 15:40:42 +0300

    feat: "mine-sweeper" task done

--- src/mine-sweeper.js
+++ src/mine-sweeper.js
@@ -23,9 +23,50 @@ const { NotImplementedError } = require('../extensions/index.js');
  *  [1, 1, 1]
  * ]
  */
-function minesweeper(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+// function minesweeper(/* matrix */) {
+//   throw new NotImplementedError('Not implemented');
+//   // remove line with error and write your code here
+// }
+function minesweeper( matrix ) {
+  // console.log(matrix);
+  let sum = 0;
+  const rowsNum = matrix.length;
+  const colsNum = matrix[0].length;
+  // const bufArr = Array(colsNum).fill(null);
+  const outArr = [];
+  let bufArr = [];
+  for (let i = 0; i < rowsNum; i++) {
+    for (let j = 0; j < colsNum; j++) {
+      m = i - 1; n = j - 1;
+      if(isCellExists(m, n, rowsNum, colsNum)) sum += Number(matrix[m][n]);
+      m = i - 1; n = j;
+      if(isCellExists(m, n, rowsNum, colsNum)) sum += Number(matrix[m][n]);
+      m = i - 1; n = j + 1;
+      if(isCellExists(m, n, rowsNum, colsNum)) sum += Number(matrix[m][n]);
+      m = i; n = j - 1;
+      if(isCellExists(m, n, rowsNum, colsNum)) sum += Number(matrix[m][n]);
+      m = i; n = j + 1;
+      if(isCellExists(m, n, rowsNum, colsNum)) sum += Number(matrix[m][n]);
+      m = i + 1; n = j - 1;
+      if(isCellExists(m, n, rowsNum, colsNum)) sum += Number(matrix[m][n]);
+      m = i + 1; n = j;
+      if(isCellExists(m, n, rowsNum, colsNum)) sum += Number(matrix[m][n]);
+      m = i + 1; n = j + 1;
+      if(isCellExists(m, n, rowsNum, colsNum)) sum += Number(matrix[m][n]);
+      bufArr.push(sum);
+      sum = 0;
+    }
+    outArr.push(bufArr);
+    bufArr = [];
+    // console.log(`Row: ${i}`, outArr);
+  }
+  // console.log(outArr);
+  return outArr;
+}
+
+function isCellExists(curRow, curCol, rowsNum, colsNum) {
+  if ((curRow >= 0) && (curRow < rowsNum) && (curCol >= 0) && (curCol < colsNum)) return true;
+  else return false;
 }
 
 module.exports = {
